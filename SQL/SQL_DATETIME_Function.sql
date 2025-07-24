ac58e7253b63036c26491a23e0d108cb51205f2c
Aquib Ansari <aquibahmed2000@gmail.com>
2025-07-24 09:57:20 +0500

    basic date-time part extraction comepleted

--- SQL/SQL_DATETIME_Function.sql
+++ SQL/SQL_DATETIME_Function.sql
@@ -24,6 +24,29 @@
 --from sales.Employees
 --select creationtime, datename(weekday,datepart(weekday, creationtime)) from sales.Orders
 
-select DATETRUNC(year, CreationTime)
-from sales.Orders
+--select DATETRUNC(year, CreationTime)
+--from sales.Orders
 
+--select datetrunc(month, creationtime),
+-- count(*) from sales.Orders
+--group by datetrunc(month, creationtime)
+
+--select eomonth(creationtime) from sales.Orders
+
+--select creationtime, eomonth(creationtime)as lastDay,datetrunc(month, creationtime) as firstDay
+--from sales.Orders
+
+
+--select DATETRUNC(month, orderdate) from sales.Orders
+
+--select DATEPART(year, creationtime), count(*) 
+--from sales.Orders
+--group by DATEPART(year, creationtime)
+
+--select datename(month,OrderDate) as month, count(*) as NumberOfOrders 
+--from sales.Orders
+--group by datename(month,OrderDate)
+
+--select orderid, datename(month,OrderDate) as month 
+--from sales.Orders
+--where month(orderdate) = 02
